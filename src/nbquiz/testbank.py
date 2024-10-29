69a962b4c0328941a81a8625424c3a09206e30b7
Mike Matera <michael.matera@cabrillo.edu>
2024-10-29 08:56:29 -0700

    Enable tesbank load from URL.

--- src/nbquiz/testbank.py
+++ src/nbquiz/testbank.py
@@ -2,13 +2,16 @@
 Interface for a group of test banks.
 """
 
+import io
 import logging
 import os
 import zipfile
 from pathlib import Path
-from typing import Iterable, Union
+from typing import Iterable
+from urllib.parse import urlparse, urlunparse
 
 import nbformat
+import requests
 
 from nbquiz.question import CellQuestion, FunctionQuestion, QuestionGroup, TestQuestion
 
@@ -23,56 +26,53 @@ class _TestBank:
         self._sources = ""
         self._paths = []
 
-    def add_path(self, path: Union[str, Path]) -> None:
-        path = Path(path).absolute()
-        if not path.exists():
-            raise ValueError(f"""Path "{path} does not exist.""")
+    def add_path(self, path: str) -> None:
         logging.info(f"Adding notebook search path: {path}")
-        self._paths.append(path)
+        self._paths.append(str(path))
 
     def load(self) -> None:
+        """Load the test banks from the collected paths."""
         for p in self._paths:
-            if p.suffix == ".zip":
-                with zipfile.ZipFile(p, "r") as zip:
-                    for file in zip.namelist():
-                        with zip.open(file) as file:
-                            self._load(file)
-                            logging.info(f"Loaded zipped testbank file: {file.name}")
+            url = urlparse(p)
+            if url.scheme.startswith("http"):
+                self._load_url(url)
             else:
-                for nb in p.glob("*.ipynb"):
-                    with open(nb) as file:
-                        self._load(nb)
-                    logging.info(f"Loaded testbank file: {nb}")
-
+                p = Path(p)
+                if not p.exists():
+                    raise ValueError("The test bank path doesn't exist:", p)
+                if p.suffix == ".zip":
+                    self._load_zip(p)
+                else:
+                    for nb in p.glob("*.ipynb"):
+                        logging.info(f"Loading test bank file: {nb}")
+                        with open(nb) as fh:
+                            self._loads(fh.read())
         logging.info(f"""Loaded {self.stats()["questions"]} questions.""")
 
-    def stats(self):
-        return {
-            "questions": len(set([id(v) for v in self._questions.values()])),
-        }
+    def _load_url(self, url):
+        """Load test questions from a test bank notebook."""
+        logging.info(f"Fetching URL: {urlunparse(url)}")
+        response = requests.get(urlunparse(url))
+        if response.status_code != 200:
+            raise ValueError(f"Error fetching url: {urlunparse(url)}: {response.status_code}")
+        if response.headers["content-type"] == "application/zip":
+            self._load_zip(io.BytesIO(response.content))
+        elif response.headers["content-type"] == "application/octet-stream":
+            self._loads(response.content)
+        else:
+            raise ValueError(f"The url {urlunparse(url)} points to something I don't understand.")
 
-    def source(self):
-        """Return a source code blob of the entire test bank."""
-        return self._sources
+    def _load_zip(self, p):
+        """Load test banks from a zip file."""
+        with zipfile.ZipFile(p, "r") as zip:
+            for file in zip.namelist():
+                with zip.open(file) as file:
+                    logging.info(f"Loading zipped test bank file: {file.name}")
+                    self._loads(file.read())
 
-    def match(self, tags: Iterable[str]) -> TestQuestion:
-        """Match a list of tags to the testbank, return a list of all matching tests."""
-        questions = [self._questions[tag] for tag in tags if tag in self._questions]
-        if not questions:
-            raise ValueError(
-                f"""I can't find a test for any of the tags: {", ".join(tags)}. Did you add the tag from the question?"""
-            )
-        return questions
-
-    def find(self, tag: str) -> TestQuestion:
-        """Find a test question by tag."""
-        found = self.match([tag])
-        if len(found) == 0:
-            raise ValueError(f"Cannot find tag: {tag}")
-        return found[0]
-
-    def _load(self, file):
-        nb = nbformat.read(file, nbformat.NO_CONVERT)
+    def _loads(self, data):
+        """Load test questions from a test bank notebook."""
+        nb = nbformat.reads(data, nbformat.NO_CONVERT)
         source = "\n\n".join(
             [
                 cell["source"]
@@ -108,6 +108,31 @@ class _TestBank:
                     self._questions[question.celltag()] = question
                     self._questions[question.cellid()] = question
 
+    def stats(self):
+        return {
+            "questions": len(set([id(v) for v in self._questions.values()])),
+        }
+
+    def source(self):
+        """Return a source code blob of the entire test bank."""
+        return self._sources
+
+    def match(self, tags: Iterable[str]) -> TestQuestion:
+        """Match a list of tags to the testbank, return a list of all matching tests."""
+        questions = [self._questions[tag] for tag in tags if tag in self._questions]
+        if not questions:
+            raise ValueError(
+                f"""I can't find a test for any of the tags: {", ".join(tags)}. Did you add the tag from the question?"""
+            )
+        return questions
+
+    def find(self, tag: str) -> TestQuestion:
+        """Find a test question by tag."""
+        found = self.match([tag])
+        if len(found) == 0:
+            raise ValueError(f"Cannot find tag: {tag}")
+        return found[0]
+
     @property
     def questions(self):
         return self._questions
@@ -120,5 +145,6 @@ class _TestBank:
 # Global singleton
 bank = _TestBank()
 
-for path in os.environ.get("NBQUIZ_TESTBANKS", "").split(":"):
-    bank.add_path(path)
+if "NBQUIZ_TESTBANKS" in os.environ:
+    for path in os.environ.get("NBQUIZ_TESTBANKS").split(":"):
+        bank.add_path(path)
