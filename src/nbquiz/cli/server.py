86c14b41bf1156aafc72a78e206bad4fddc4d8da
Mike Matera <michael.matera@cabrillo.edu>
2025-06-12 10:20:07 -0700

    Graceful shutdown on sigint.

--- src/nbquiz/cli/server.py
+++ src/nbquiz/cli/server.py
@@ -12,6 +12,9 @@ from nbquiz.testbank import bank
 
 from ..runtime.server import Checker, checker_pb2_grpc
 
+# Add coroutines here to perform cleanup on shutdown.
+cleanup_handlers = []
+
 
 def add_args(parser: ArgumentParser):
     parser.add_argument(
@@ -31,6 +34,8 @@ def add_args(parser: ArgumentParser):
 
 
 async def run_server(args: ArgumentParser) -> None:
+    global cleanup_handlers
+
     server = grpc.aio.server(maximum_concurrent_rpcs=args.concurrent)
     checker = Checker()
     checker_pb2_grpc.add_CheckerServicer_to_server(checker, server)
@@ -38,14 +43,26 @@ async def run_server(args: ArgumentParser) -> None:
     server.add_insecure_port(listen_addr)
     if args.concurrent is not None:
         logging.info(f"Set concurrency limit of {args.concurrent} RPCs.")
-    logging.info("Starting server on %s", listen_addr)
+
     await server.start()
-    await server.wait_for_termination()
+    logging.info("Starting server on %s", listen_addr)
+
+    async def stop_server():
+        logging.info("Shutting down server.")
+        await server.stop(None)
+
+    cleanup_handlers.append(stop_server())
+
+    try:
+        await server.wait_for_termination()
+    finally:
+        async with asyncio.TaskGroup() as tg:
+            for h in cleanup_handlers:
+                tg.create_task(h)
 
 
 def main(args: ArgumentParser):
     # Validate paths so that errors happen sooner rather than later.
     bank.load()
-
     logging.basicConfig(level=logging.INFO)
     asyncio.run(run_server(args))
